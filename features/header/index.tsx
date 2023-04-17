f7c07a305993982683469d63cf2052a614bcac87
sadeq <s.lotfi@fanap.ir>
2023-04-17 13:27:57 +0330

    project initialize

---
+++ features/header/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="relative w-20 h-20">
+        <Image src="" fill alt="addmin logo" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
