2e3b19822dd487f9fb67c6469210eac47699b356
sadeq <s.lotfi@fanap.ir>
2023-04-19 00:36:40 +0330

    animation text effect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import Header from "@/features/header";
 import style from "./Home.module.css";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="px-22 pt-62">
+      <main className="pt-62">
         {/* /* ------------------------------ Hero Section ------------------------------  */}
-        <div className="flex justify-between">
+        <div className="px-22 flex justify-between">
           <div className="w-184 h-188 flex-col flex justify-between">
             <h2 className={style.tagline}>
               <span className={style["tagline-span"]}> add</span> an admin in a{" "}
@@ -38,7 +39,7 @@ export default function Home() {
           </div>
         </div>
         {/* /* ------------------------------- 2nd section ------------------------------  */}
-        <div className="mt-90 px-14">
+        <div className="mt-90 px-36 pb-92">
           <p className="text-main-orange font-InterExtraBold text-7.5xl">
             2,900,000
           </p>
@@ -53,7 +54,7 @@ export default function Home() {
             -- crunchbase 2020
           </p>
           {/* toggle button and motto */}
-          <div className="mt-108 flex justify-center">
+          <div className="mt-108 flex justify-center items-center gap-9">
             <div className="relative w-35 h-17">
               <Image
                 src="/assets/images/toggle-button.svg"
@@ -61,7 +62,24 @@ export default function Home() {
                 alt="addmin logo and toggle button"
               />
             </div>
+            <p className="font-InterExtraBold text-7.5xl">
+              <span>Don’t share your</span>{" "}
+              <TypeAnimation
+                sequence={["Life", 1000, "Lifeee", 1000, "Lifeeeeee", 1000]}
+                repeat={Infinity}
+                style={{ color: "#FF782C" }}
+              />
+            </p>
           </div>
+        </div>
+        {/* /* ------------------------------- 3rd section ------------------------------  */}
+        <div className="bg-white pt-48">
+          <p className="font-InterExtraBold text-7.5xl text-black text-center mb-58">
+            Easily manage as a{" "}
+            <span className="font-InterExtraBold text-7.5xl text-main-orange">
+              Toggle!
+            </span>
+          </p>
         </div>
       </main>
     </>
