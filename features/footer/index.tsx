897a0c5c1c6983d9e3a1af065637b754bd753092
Erfan <erffon@gmail.com>
2023-04-20 02:51:55 +0330

    footer done

--- features/footer/index.tsx
+++ features/footer/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="pt-52 px-40">
+    <footer className="pt-52 px-40 pb-22">
       <div className="flex justify-between items-start pb-48">
         <div>
           <h3 className="font-InterExtraBold text-7.5xl">
@@ -15,6 +15,7 @@ const Footer = () => {
             <RxInstagramLogo className="hover:scale-110 transition duration-300 ease-out" />
           </div>
         </div>
+        {/* -------------------------------------------------------------------------------------- */}
         <div>
           <div className="relative w-104 h-32 mt-7 hover:scale-95 duration-300 ease-out transition">
             <Image fill src="/assets/images/pwa-cta.svg" alt="pwa cta" />
@@ -26,6 +27,27 @@ const Footer = () => {
             <Image fill src="/assets/images/googleplay-cta.svg" alt="pwa cta" />
           </div>
         </div>
+      </div>
+      <div className="flex gap-10">
+        <p className="font-InterBold text-2xl">Copyright ©2023 addmin</p>
+        <a
+          href=""
+          className="text-main-orange font-InterBold text-2xl underline"
+        >
+          Privacy
+        </a>
+        <a
+          href=""
+          className="text-main-orange font-InterBold text-2xl underline"
+        >
+          Terms
+        </a>
+        <a
+          href=""
+          className="text-main-orange font-InterBold text-2xl underline"
+        >
+          Security
+        </a>
       </div>
     </footer>
   );
